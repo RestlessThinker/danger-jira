1468500c59a135682274bc5fe43d27ec75d11e43
Mikko Kokkonen <mikko.kokkonen@appearhere.co.uk>
2018-02-09 14:32:09 +0000

    JIRA Tickets in Commits
    Find JIRA Ticket mentions from commits as well and not just PR title

--- lib/jira/plugin.rb
+++ lib/jira/plugin.rb
@@ -25,19 +25,19 @@ module Danger
     #
     # @return [void]
     #
-    def check(key: nil, url: nil, emoji: ":link:", fail_on_warning: false)
+    def check(key: nil, url: nil, emoji: ":link:", fail_on_warning: false, report_missing: true)
       throw Error("'key' missing - must supply JIRA issue key") if key.nil?
       throw Error("'url' missing - must supply JIRA installation URL") if url.nil?
 
       # Support multiple JIRA projects
       keys = key.kind_of?(Array) ? key.join("|") : key
-      jira_key_regex_string = "((#{keys})-[0-9]+)"
+      jira_key_regex_string = "((?:#{keys})-[0-9]+)"
       regexp = Regexp.new(/#{jira_key_regex_string}/)
 
-      jira_issues = []
-      github.pr_title.gsub(regexp) do |match|
-        jira_issues << match
-      end
+      jira_issues = [
+        github.pr_title.scan(regexp),
+        git.commits.map { |commit| commit.message.scan(regexp) }
+      ].flatten.uniq
 
       if jira_issues.empty?
         github.pr_body.gsub(regexp) do |match|
@@ -48,8 +48,8 @@ module Danger
       if !jira_issues.empty?
         jira_urls = jira_issues.map { |issue| link(href: ensure_url_ends_with_slash(url), issue: issue) }.join(", ")
         message("#{emoji} #{jira_urls}")
-      else
-        msg = "This PR does not contain a JIRA issue key in the PR title (e.g. KEY-123)"
+      elsif report_missing
+        msg = "This PR does not contain any JIRA issue keys in the PR title or commit messages (e.g. KEY-123)"
         if fail_on_warning
           fail(msg)
         else
